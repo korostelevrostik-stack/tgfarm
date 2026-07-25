e91ddc9ecadefa56a8d661f3df5e266aa84c0ea6
korostelevrostik-stack <korostelevrostik@gmail.com>
2026-07-25 16:11:29 +0500

    Add admin handler registration

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,11 +1,10 @@
-# handlers/__init__.py
-from . import start, plant, harvest, animals, day, stats
+from . import start, plant, harvest, animals, day, stats, admin
 
 def register_all_handlers(dp):
-    """Регистрируем все обработчики команд и колбэков"""
     start.register_start(dp)
     plant.register_plant(dp)
     harvest.register_harvest(dp)
     animals.register_animals(dp)
     day.register_day(dp)
     stats.register_stats(dp)
+    admin.register_admin(dp)
